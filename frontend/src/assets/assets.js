dcffd391ecc83d555f2cd1b888dd7ba31774954b
Wycliffe Omondi <odhiambow2354@gmail.com>
2025-02-09 11:30:44 +0300

    initialize frontend project with Vite, React, and essential assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,282 @@
+import logo from "./logo.png";
+import cart_icon from "./cart_icon.png";
+import food_dev from "./food_dev.jpg";
+import header_img from "./header_img.png";
+import search_icon from "./search_icon.png";
+import cat from "./cat.png";
+import dog from "./dog.png";
+import rabbit from "./rabbit.png";
+import fish from "./fish.png";
+import bird from "./bird.png";
+import banner1 from "./banner1.png";
+
+//pets
+import b1 from "./b1.jpg";
+import b2 from "./food_2.jpg";
+import b3 from "./b3.png";
+import b4 from "./b4.png";
+import b5 from "./b5.png";
+import c from "./c.png";
+import c1 from "./c1.png";
+import c2 from "./c2.png";
+import c3 from "./c3png";
+import c4 from "./c4.png";
+import c5 from "./c5.png";
+import d from "./d.png";
+import d1 from "./d1.png";
+import d2 from "./d2.png";
+import d3 from "./d3.png";
+import d4 from "./d4.png";
+import r1 from "./r1.png";
+import r2 from "./r2.png";
+import r3 from "./r3.png";
+import f1 from "./f1.png";
+import f2 from "./f2.png";
+import f3 from "./f3.png";
+import f4 from "./f4.png";
+
+//pet food
+import bird1 from "./bird1.png";
+import bird2 from "./bird2.png";
+import bird3 from "./bird3.png";
+import both from "./both.png";
+import cat1 from "./cat1.png";
+import cat2 from "./cat2.png";
+import cat3 from "./cat3.png";
+import dog1 from "./dog1.png";
+import dog2 from "./dog2.png";
+import fish1 from "./fish1.png";
+import fish2 from "./fish2.png";
+import fish3 from "./fish3.png";
+import fish4 from "./fish4.png";
+import fish5 from "./fish5.png";
+import fish6 from "./fish6.png";
+import fish7 from "./fish7.png";
+import rabbit1 from "./rabbit1.png";
+import rabbit2 from "./rabbit2.png";
+
+import add_icon_white from "./add_icon_white.png";
+import add_icon_green from "./add_icon_green.png";
+import remove_icon_red from "./remove_icon_red.png";
+import app_store from "./app_store.png";
+import play_store from "./play_store.png";
+import linkedin_icon from "./linkedin_icon.png";
+import facebook_icon from "./facebook_icon.png";
+import twitter_icon from "./twitter_icon.png";
+import cross_icon from "./cross_icon.png";
+import selector_icon from "./selector_icon.png";
+import rating_starts from "./rating_starts.png";
+import profile_icon from "./profile_icon.png";
+import logout_icon from "./logout_icon.png";
+import parcel_icon from "./parcel_icon.png";
+
+export const assets = {
+  logo,
+  header_img,
+  banner1,
+  search_icon,
+  rating_starts,
+  add_icon_green,
+  add_icon_white,
+  remove_icon_red,
+  app_store,
+  play_store,
+  linkedin_icon,
+  facebook_icon,
+  twitter_icon,
+  cross_icon,
+  selector_icon,
+  profile_icon,
+  logout_icon,
+  parcel_icon,
+  cart_icon,
+  food_dev,
+};
+
+export const pet_list = [
+  {
+    pet_name: "Cat",
+    pet_image: cat,
+  },
+  {
+    pet_name: "Dog",
+    pet_image: dog,
+  },
+  {
+    pet_name: "Bird",
+    pet_image: bird,
+  },
+  {
+    pet_name: "Fish",
+    pet_image: fish,
+  },
+  {
+    pet_name: "Rabbit",
+    pet_image: rabbit,
+  },
+];
+
+export const food_list = [
+  {
+    id: "1",
+    name: "Hartz Delectables Stew",
+    image: bird1,
+    price: 102,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bird",
+  },
+  {
+    id: "2",
+    name: "Zupreem Fruit Blend",
+    image: bird2,
+    price: 108,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bird",
+  },
+  {
+    id: "3",
+    name: "Kaytee Forti-Diet Pro Health",
+    image: bird3,
+    price: 106,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bird",
+  },
+  {
+    id: "4",
+    name: "Mazuri Small Bird Food",
+    image: both,
+    price: 204,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Bird",
+  },
+  {
+    id: "5",
+    name: "Purina Friskies",
+    image: cat1,
+    price: 104,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cat",
+  },
+  {
+    id: "6",
+    name: "Meow Mix",
+    image: cat2,
+    price: 102,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cat",
+  },
+  {
+    id: "7",
+    name: "Purina Felix Original",
+    image: cat3,
+    price: 200,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cat",
+  },
+  {
+    id: "8",
+    name: "Pedigree",
+    image: dog1,
+    price: 105,
+    description:
+      "Chunky ground dinner with chicken. Food provides essential nutrients for overall health and well-being",
+    category: "Dog",
+  },
+  {
+    id: "9",
+    name: "Pedigree",
+    image: dog2,
+    price: 140,
+    description:
+      "Choice cuts in gravy with beef. Food provides essential nutrients for overall health and well-being",
+    category: "Deserts",
+  },
+  {
+    id: "10",
+    name: "Rascally Rat Nutri-Berries",
+    image: rabbit1,
+    price: 220,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rabbit",
+  },
+  {
+    id: "11",
+    name: "Nutrilogic",
+    image: rabbit2,
+    price: 100,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rabbit",
+  },
+  {
+    id: "12",
+    name: "Tetra Goldfish",
+    image: fish1,
+    price: 120,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Fish",
+  },
+  {
+    id: "13",
+    name: "API Goldfish Pellets",
+    image: fish2,
+    price: 120,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Fish2",
+  },
+  {
+    id: "14",
+    name: "GloFish",
+    image: fish3,
+    price: 180,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Fish",
+  },
+  {
+    id: "15",
+    name: "Aqueon Shrimp Pellets",
+    image: fish4,
+    price: 160,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Fish",
+  },
+  {
+    id: "16",
+    name: "FishScience",
+    image: fish5,
+    price: 240,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Fish",
+  },
+  {
+    id: "17",
+    name: "Maf Special",
+    image: fish6,
+    price: 140,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Fish",
+  },
+  {
+    id: "18",
+    name: "King British",
+    image: fish7,
+    price: 120,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Fish",
+  },
+];
